517fe28fa775b2fa10b2033f487f8842e706d3b8
Tyson <tysonpederson95@gmail.com>
2023-11-06 21:41:14 -0500

    Implement the random recipe generator microservice

---
+++ example_call.js
@@ -0,0 +1,21 @@
+const apiKey = "c234792adad6494fbf71c9053e670b02";    // Input your API key here from your Spoonacular profile
+
+// An example call that gets a list of 5 recipes from Spoonacular's Find By Ingredients API and prints out a randomly chosen recipe's detail instructions in string format
+  // https://spoonacular.com/food-api/docs#Search-Recipes-by-Ingredients
+async function exampleCall() {
+  // Get some recipes from Spoonacular and build a stringified list of recipe IDs
+  const spoonReq = await fetch(`https://api.spoonacular.com/recipes/findByIngredients?ingredients=apples,+flour,+sugar&number=5&apiKey=${apiKey}`);
+  const spoonRes = await spoonReq.json();
+  let recipeIDs = [];
+  for (const recipe of spoonRes) {
+    recipeIDs.push(recipe["id"]);
+  }
+  recipeIDs = JSON.stringify(recipeIDs);
+
+  // Call the microservice to get the detailed recipe info of a randomly chosen recipe from the list of recipeIDs
+  const response = await fetch(`http://localhost:8000?recipes=${recipeIDs}`);
+  const data = await response.json();
+  console.log(JSON.stringify(data));
+}
+
+exampleCall();
---
+++ microservice.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const port = 8000;
+const apiKey = "c234792adad6494fbf71c9053e670b02";    // Input your API key here from your Spoonacular profile
+
+// Randomly choses a recipe ID in the list of recipe IDs given
+// Returns the recipe ID and the Get Analyzed Recipe Instructions API response from Spoonacular for that randomly chosen recipe
+  // https://spoonacular.com/food-api/docs#Get-Analyzed-Recipe-Instructions
+async function getRandomRecipe(recipeIDs) {
+  recipeIDs = JSON.parse(recipeIDs);
+  const recipeID = recipeIDs[Math.floor(Math.random() * recipeIDs.length)].toString();;
+  const response = await fetch(`https://api.spoonacular.com/recipes/${recipeID}/analyzedInstructions?apiKey=${apiKey}`);
+  const result = await response.json();
+  return {"recipeID":recipeID, "instructions":result};
+}
+
+// Sends back the recipe ID and detailed recipe instructions for a randomly chosen recipe in the given list of recipes
+app.get("/", async function (req, res) {
+  res.json(await getRandomRecipe(req.query["recipes"]));
+});
+
+app.listen(port, function () {
+  console.log(`Microservice listening on port ${port}!`);
+});
